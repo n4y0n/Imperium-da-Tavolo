9386e36e90ed438a5ad73d26720be47032fedb76
Ernesto Montada Romero <e.montada0@gmail.com>
2021-12-19 11:47:28 +0100

    Added WIP Text on the bottom right of the screen

---
+++ src/components/WIP.jsx
@@ -0,0 +1,5 @@
+export default () => {
+    return (
+        <div className="font-bold text-9xl text-red-400 fixed bottom-2 right-2">WIP</div>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import WIP from './components/WIP'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <WIP />
   </React.StrictMode>,
   document.getElementById('root')
 )
